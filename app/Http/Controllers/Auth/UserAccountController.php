4f9aa40357a8fcb61b29b4465095d9f88068b996
erikmartins <erik.ozzetti.martins@gmail.com>
2023-02-26 17:00:16 -0300

    feat: UserAccountController

---
+++ app/Http/Controllers/Auth/UserAccountController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\View\View;
+
+class UserAccountController extends Controller
+{
+    public function  signIn () :View
+    {
+        return  view('auth.sign-in');
+    }
+
+    public function  store (UserAccountStoreRequest $request)
+    {
+
+        $request->valided();
+
+        $emailExists = User::where('email', $request->email)-> first();
+        if($emailExists) {
+            return back()->withErrors([
+                'email' => 'Email ja cadastrado'
+            ]);
+        }
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' =>  Hash::make( $request->password )
+        ]);
+
+
+        return  redirect()->route('sign-in');
+
+
+    }
+}
